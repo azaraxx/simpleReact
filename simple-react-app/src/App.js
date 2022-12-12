99e221716e3e840f209c056eb399e32d233727f8
azaraxx <azaraxx@hotmail.com>
2022-12-12 22:47:19 +0300

    simple add/remove user app

--- simple-react-app/src/App.js
+++ simple-react-app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import AddUser from "./components/AddUser";
+import Users from "./components/Users";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      users: [
+        {
+          id: 1,
+          name: "Lionel Messi",
+          email: "test@test.com",
+        },
+        {
+          id: 2,
+          name: "Cristiano Ronaldo",
+          email: "test@test.com",
+        },
+        {
+          id: 3,
+          name: "Kylian Mbappe",
+          email: "test@test.com",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h4>User App</h4>
+        <hr />
+        <AddUser />
+        <hr/>
+        <Users />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ simple-react-app/src/components/Users.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+  render() {
+    return (
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Users;
